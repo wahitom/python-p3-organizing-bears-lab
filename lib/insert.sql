596fbcb8038943b6de3f0f08590aa426a24893d8
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-12-07 16:35:18 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO bears (bear_name, age, sex, color, temparament, alive) VALUES ("Mr Chocolate", 20, "female", "brown", "chill", TRUE);
+INSERT INTO bears (bear_name, age, sex, color, temparament, alive) VALUES ("Wendy", 2, "female", "brown", "sassy", TRUE);
+INSERT INTO bears (bear_name, age, sex, color, temparament, alive) VALUES ("Rowdy", 12, "female", "white", "angry", TRUE);
+INSERT INTO bears (bear_name, age, sex, color, temparament, alive) VALUES ("Tabitha", 14, "female", "brown", "nice on a sunny day", TRUE);
+INSERT INTO bears (bear_name, age, sex, color, temparament, alive) VALUES ("Melissa", 5, "female", "white", "happy", TRUE);
+INSERT INTO bears (bear_name, age, sex, color, temparament, alive) VALUES ("Grinch", 3, "male", "brown", "grupmy", TRUE);
